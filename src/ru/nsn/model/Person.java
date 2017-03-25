e5d1947b1efa4e17a511fd4329552646627e86fb
bn <borpernic@gmail.com>
2017-03-26 03:06:04 +0400

    all project

--- src/ru/nsn/model/Person.java
+++ src/ru/nsn/model/Person.java
@@ -1,145 +1,138 @@
 package ru.nsn.model;
 
-import java.time.LocalDate;
+import javafx.beans.property.*;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import java.time.LocalDate;
 
 public class Person {
 
-	private final StringProperty firstName;
-	private final StringProperty lastName;
-	private final StringProperty street;
-	private final IntegerProperty postalCode;
-	private final StringProperty city;
-	private final ObjectProperty<LocalDate> birthday;
-	private final IntegerProperty age;
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty street;
+    private final IntegerProperty postalCode;
+    private final StringProperty city;
+    private final ObjectProperty<LocalDate> birthday;
+    private final IntegerProperty age;
 
-	public Person() {
-		this(null, null);
-	}
+    public Person() {
+        this(null, null);
+    }
 
-	public Person(String firstName, String lastName) {
-		this.firstName = new SimpleStringProperty(firstName);
-		this.lastName = new SimpleStringProperty(lastName);
-		this.age = new SimpleIntegerProperty(1234);
-		// Какие-то фиктивные начальные данные для удобства тестирования.
-		this.street = new SimpleStringProperty("какая-то улица");
-		this.postalCode = new SimpleIntegerProperty(1234);
-		this.city = new SimpleStringProperty("какой-то город");
-		this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
-	}
+    public Person(String firstName, String lastName) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.age = new SimpleIntegerProperty(1234);
+        // Какие-то фиктивные начальные данные для удобства тестирования.
+        this.street = new SimpleStringProperty("какая-то улица");
+        this.postalCode = new SimpleIntegerProperty(1234);
+        this.city = new SimpleStringProperty("какой-то город");
+        this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
+    }
 
-	public final StringProperty firstNameProperty() {
-		return this.firstName;
-	}
-	
+    public final StringProperty firstNameProperty() {
+        return this.firstName;
+    }
 
-	public final String getFirstName() {
-		return this.firstNameProperty().get();
-	}
-	
 
-	public final void setFirstName(final String firstName) {
-		this.firstNameProperty().set(firstName);
-	}
-	
+    public final String getFirstName() {
+        return this.firstNameProperty().get();
+    }
 
-	public final StringProperty lastNameProperty() {
-		return this.lastName;
-	}
-	
 
-	public final String getLastName() {
-		return this.lastNameProperty().get();
-	}
-	
+    public final void setFirstName(final String firstName) {
+        this.firstNameProperty().set(firstName);
+    }
 
-	public final void setLastName(final String lastName) {
-		this.lastNameProperty().set(lastName);
-	}
-	
 
-	public final StringProperty streetProperty() {
-		return this.street;
-	}
-	
+    public final StringProperty lastNameProperty() {
+        return this.lastName;
+    }
 
-	public final String getStreet() {
-		return this.streetProperty().get();
-	}
-	
 
-	public final void setStreet(final String street) {
-		this.streetProperty().set(street);
-	}
-	
+    public final String getLastName() {
+        return this.lastNameProperty().get();
+    }
 
-	public final IntegerProperty postalCodeProperty() {
-		return this.postalCode;
-	}
-	
 
-	public final int getPostalCode() {
-		return this.postalCodeProperty().get();
-	}
-	
+    public final void setLastName(final String lastName) {
+        this.lastNameProperty().set(lastName);
+    }
 
-	public final void setPostalCode(final int postalCode) {
-		this.postalCodeProperty().set(postalCode);
-	}
-	
 
-	public final StringProperty cityProperty() {
-		return this.city;
-	}
-	
+    public final StringProperty streetProperty() {
+        return this.street;
+    }
 
-	public final String getCity() {
-		return this.cityProperty().get();
-	}
-	
 
-	public final void setCity(final String city) {
-		this.cityProperty().set(city);
-	}
-	
+    public final String getStreet() {
+        return this.streetProperty().get();
+    }
 
-	public final ObjectProperty<LocalDate> birthdayProperty() {
-		return this.birthday;
-	}
-	
 
-	public final LocalDate getBirthday() {
-		return this.birthdayProperty().get();
-	}
-	
+    public final void setStreet(final String street) {
+        this.streetProperty().set(street);
+    }
 
-	public final void setBirthday(final LocalDate birthday) {
-		this.birthdayProperty().set(birthday);
-	}
 
-	public final IntegerProperty ageProperty() {
-		return this.age;
-	}
-	
+    public final IntegerProperty postalCodeProperty() {
+        return this.postalCode;
+    }
 
-	public final int getAge() {
-		return this.ageProperty().get();
-	}
-	
 
-	public final void setAge(final int age) {
-		this.ageProperty().set(age);
-	}
-	
-	//ВВАПВАПВКАП ert r
-		
-	
+    public final int getPostalCode() {
+        return this.postalCodeProperty().get();
+    }
+
+
+    public final void setPostalCode(final int postalCode) {
+        this.postalCodeProperty().set(postalCode);
+    }
+
+
+    public final StringProperty cityProperty() {
+        return this.city;
+    }
+
+
+    public final String getCity() {
+        return this.cityProperty().get();
+    }
+
+
+    public final void setCity(final String city) {
+        this.cityProperty().set(city);
+    }
+
+
+    public final ObjectProperty<LocalDate> birthdayProperty() {
+        return this.birthday;
+    }
+
+
+    public final LocalDate getBirthday() {
+        return this.birthdayProperty().get();
+    }
+
+
+    public final void setBirthday(final LocalDate birthday) {
+        this.birthdayProperty().set(birthday);
+    }
+
+    public final IntegerProperty ageProperty() {
+        return this.age;
+    }
+
+
+    public final int getAge() {
+        return this.ageProperty().get();
+    }
+
+
+    public final void setAge(final int age) {
+        this.ageProperty().set(age);
+    }
+
+    //ВВАПВАПВКАП ert r
+
+
 }
--- src/ru/nsn/view/MainApp.java
+++ src/ru/nsn/view/MainApp.java
@@ -14,112 +14,112 @@ import ru.nsn.model.Person;
 import java.io.IOException;
 
 public class MainApp extends Application {
-	private Stage primaryStage;
+    private Stage primaryStage;
 
-	private BorderPane rootLayout;
-	private ObservableList<Person> personData = FXCollections.observableArrayList();
+    private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
-	public MainApp() {
-		personData.add(new Person("Hans", "Muster"));
-		personData.add(new Person("Ruth", "Mueller"));
-		personData.add(new Person("Heinz", "Kurz"));
-		personData.add(new Person("Cornelia", "Meier"));
-		personData.add(new Person("Werner", "Meyer"));
-		personData.add(new Person("Lydia", "Kunz"));
-		personData.add(new Person("Anna", "Best"));
-		personData.add(new Person("Stefan", "Meier"));
-		personData.add(new Person("Martin", "Mueller"));
-		personData.add(new Person("Борис", "Першин"));
+    public MainApp() {
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+        personData.add(new Person("Борис", "Першин"));
 
-	}
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	public ObservableList<Person> getPersonData() {
-		return personData;
-	}
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("NSN приложение");
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("NSN приложение");
 
-		initRootLayout();
+        initRootLayout();
 
-		showPersonOverview();
+        showPersonOverview();
 
-	}
+    }
 
-	public void initRootLayout() {
-		try {
-			// Загружаем корневой макет из fxml файла.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("../view/RootLayout.fxml"));
-			rootLayout = (BorderPane) loader.load();
+    public void initRootLayout() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("../view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
 
-			// Отображаем сцену, содержащую корневой макет.
-			Scene scene = new Scene(rootLayout);
-			//scene.getStylesheets().add("../view/Nsn.css");//.styleProperty().setValue(
-			//scene.getRoot().css
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            //scene.getStylesheets().add("../view/Nsn.css");//.styleProperty().setValue(
+            //scene.getRoot().css
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void showPersonOverview() {
-		try {
-			// Загружаем сведения об адресатах.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("SiteOverview.fxml"));
-			AnchorPane personOverview = (AnchorPane) loader.load();
+    public void showPersonOverview() {
+        try {
+            // Загружаем сведения об адресатах.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("SiteOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
 
-			// Помещаем сведения об адресатах в центр корневого макета.
-			rootLayout.setCenter(personOverview);
-			//rootLayout.setBottom(personOverview);
-			PersonOverviewController controller = loader.getController();
-			controller.setMainApp(this);
+            // Помещаем сведения об адресатах в центр корневого макета.
+            rootLayout.setCenter(personOverview);
+            //rootLayout.setBottom(personOverview);
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
-	
-	public boolean showPersonEditDialog(Person person) {
-	    try {
-	        // Загружаем fxml-файл и создаём новую сцену
-	        // для всплывающего диалогового окна.
-	        FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(MainApp.class.getResource("dialogEdit.fxml"));
-	        AnchorPane page = (AnchorPane) loader.load();
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
-	        // Создаём диалоговое окно Stage.
-	        Stage dialogStage = new Stage();
-	        dialogStage.setTitle("Edit Person");
-	        dialogStage.initModality(Modality.WINDOW_MODAL);
-	        dialogStage.initOwner(primaryStage);
-	        Scene scene = new Scene(page);
-	        dialogStage.setScene(scene);
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Загружаем fxml-файл и создаём новую сцену
+            // для всплывающего диалогового окна.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("dialogEdit.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
 
-	        // Передаём адресата в контроллер.
-	        PersonEditDialogController controller = loader.getController();
-	        controller.setDialogStage(dialogStage);
-	        controller.setPerson(person);
+            // Создаём диалоговое окно Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
 
-	        // Отображаем диалоговое окно и ждём, пока пользователь его не закроет
-	        dialogStage.showAndWait();
+            // Передаём адресата в контроллер.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
 
-	        return controller.isOkClicked();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return false;
-	    }
-	}
+            // Отображаем диалоговое окно и ждём, пока пользователь его не закроет
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
