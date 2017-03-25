96a94d4a8f8340e9472841ad488c61089d4a9b8e
bn <borpernic@gmail.com>
2017-03-25 12:46:29 +0300

    all project added

--- src/ru/nsn/interfaces/SiteFactory.java
+++ src/ru/nsn/interfaces/SiteFactory.java
@@ -3,7 +3,7 @@ package ru.nsn.interfaces;
 import ru.nsn.model.Site;
 
 /**
- * Created by ${Borpernic} on 24.03.2017.
+ * Created by Borpernic on 24.03.2017.
  */
 
 public interface SiteFactory {
@@ -12,4 +12,5 @@ public interface SiteFactory {
     void  updateSite(Site site);
     void  deleteSaite(long siteId);
     Site selectSite(long siteId);
+
 }
